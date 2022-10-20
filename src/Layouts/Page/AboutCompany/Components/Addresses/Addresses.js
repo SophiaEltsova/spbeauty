64dc51cfdd607968957c592c2230f1fbf9f35234
Sophia Eltsova <s.eltsova@bk.ru>
2022-10-21 02:31:41 +0300

    initial commit

--- src/Layouts/Page/AboutCompany/Components/Addresses/Addresses.js
+++ src/Layouts/Page/AboutCompany/Components/Addresses/Addresses.js
@@ -1,17 +1,25 @@
-import PrimaryButton from "../../../../../Components/PrimaryButton";
 import SecondaryButton from "../../../../../Components/SecondaryButton";
 import ListElement from "./ListElement";
+import styled from "styled-components";
+
 function Addresses() {
-    return (
-        <div className='d-inline-flex flex-column gap-2 mt-4'>
-            <div className='text-rg'>Адреса заведений</div>
-            <div className='d-inline-flex flex-column gap-3'>
-                <ListElement>Санкт-Петербург, Невский пр-т. 28</ListElement>
-                <ListElement>Санкт-Петербург, Казанская ул, 7</ListElement>
-                <PrimaryButton>Добавить филиал</PrimaryButton>
-            </div>
+  const AddressesComponent = styled.div`
+    border-radius: 24px;
+    border: 1px solid rgba(100, 116, 139, 0.12);
+    padding: 24px;
+  `;
+  return (
+    <AddressesComponent>
+      <div className="d-flex flex-column gap-4 justify-content-between">
+        <div className="title">Адреса заведений</div>
+        <div className="d-inline-flex flex-column gap-2">
+          <ListElement>Санкт-Петербург, Невский пр-т. 28</ListElement>
+          <ListElement>Санкт-Петербург, Казанская ул, 7</ListElement>
+          <SecondaryButton>Добавить филиал</SecondaryButton>
         </div>
-    );
+      </div>
+    </AddressesComponent>
+  );
 }
 
 export default Addresses;
--- src/Layouts/Page/AboutCompany/Components/Addresses/ListElement.js
+++ src/Layouts/Page/AboutCompany/Components/Addresses/ListElement.js
@@ -1,25 +1,17 @@
-import styled from 'styled-components';
-import SecondaryButton from '../../../../../Components/SecondaryButton';
-import EditIcon from '../../../../../Icons/EditIcon';
-import RemoveIcon from '../../../../../Icons/RemoveIcon';
-import IconOnlyButton from '../../../../../Components/IconOnlyButton.js';
+import styled from "styled-components";
+import EditIcon from "../../../../../Icons/EditIcon";
+import RemoveIcon from "../../../../../Icons/RemoveIcon";
 
-const Option = styled.div`
-    outline: none;
-    border-radius: 0px;
-    border: solid #152842 1px;
-    background-color: #D9E2EF;
-    color: #666787;
-`;
+const Option = styled.div``;
 
 function ListElement(props) {
-    return (
-        <div className='d-flex flex-row align-items-center align-self-stretch gap-2'>
-            <Option className='flex-grow-1 p-2'>{props.children}</Option>
-            <SecondaryButton minWidth='0'>Ред.</SecondaryButton>
-            <SecondaryButton minWidth='0'>Удл.</SecondaryButton>
-        </div>
-    );
+  return (
+    <div className="d-flex flex-row align-items-center align-self-stretch gap-2"> 
+      <Option className="flex-grow-1">{props.children}</Option>
+      <EditIcon minWidth="0">Ред.</EditIcon>
+      <RemoveIcon minWidth="0">Удл.</RemoveIcon>
+    </div>
+  );
 }
 
 export default ListElement;
